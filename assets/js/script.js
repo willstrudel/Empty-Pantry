99226adb2596581a55908f2ce70f6fb80b2e06ca
Natnael Haile <natnaelhaile@Natnaels-MacBook-Pro.local>
2021-07-01 11:05:49 -0400

    completed mini detail button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 var calendarEl = $("#datepicker");
 var selectedDate;
 var resultCardContainer = $(".result-card-container");
-//Search Button
-let searchBtn = document.querySelector("#search");
+var searchBtn = document.querySelector("#search");
 
 //Event Listener to search for recipe
 searchBtn.addEventListener("click", searchReceipeData);
@@ -181,6 +180,7 @@ function loadShoppingCart() {
   }
 }
 
+//Create saved ingredient content
 function saveToList(key) {
   var localItem = localStorage.getItem(key);
   var localItem_object = JSON.parse(localItem);
@@ -225,6 +225,7 @@ ingredientPicked.on("click", ".miniDetailBtn", function (e) {
   $("#myModal").modal("show");
 });
 
+//EVENT LISTENER FOR SAVE BUTTON
 resultCardContainer.on("click", ".saveBtn", function () {
   var cardDetail = $(this).siblings().last();
   var index = cardDetail.attr("data-index");
@@ -283,59 +284,3 @@ ingredientPicked.on("click", ".removeBtn", function () {
   );
   $(this).parent().remove();
 });
-
-
-
-
-
-//Animation Typing Carousel (Code below copied from https://codepen.io/gschier/pen/jkivt)
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-  if (this.isDeleting) { delta /= 2; }
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
-};
